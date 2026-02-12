0dc968bca9c4773cdb2edb8e98e6ca6b7fcd08c4
Ngungila <168288981+Ngungila@users.noreply.github.com>
2026-02-12 13:25:48 +0300

    Fix AdminController middleware error - remove constructor middleware as it's applied in routes

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware(['auth', 'role:admin']);
-    }
-
     /**
      * Admin Dashboard
      */
